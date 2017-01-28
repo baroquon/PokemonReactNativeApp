5f46a39b71c323f09852c2ebd2943199e1d526dd
Brandon Blaylock <brandon.blaylock@gmail.com>
2017-01-27 21:21:15 -0600

    a working App

---
+++ models/Pokemon.js
@@ -0,0 +1,9 @@
+export default class Pokemon {
+  constructor(name, weight, height, image, pokeType) {
+    this.name = name;
+    this.weight = weight;
+    this.height = height;
+    this.image_url = image;
+    this.pokeType = pokeType;
+  }
+}
